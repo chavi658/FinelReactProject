308d99afea72fd062afa39e45a130166f255be84
chavi658 <148474166+chavi658@users.noreply.github.com>
2024-03-26 12:57:03 +0200

    Update addUser.js

--- src/user/addUser.js
+++ src/user/addUser.js
@@ -1,11 +1,7 @@
 import { useState } from "react";
 
 const AddUser=(data)=>{
-// const [users,setUsers]=useState([]);
 
-//     const newUser = [...users];
-//     newPost.push(data);
-//     setUsers(newUser);
    return
    (<>
    <p>welcome {data?.userName}</p>
